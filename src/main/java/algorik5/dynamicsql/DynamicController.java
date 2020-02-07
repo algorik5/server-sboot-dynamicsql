030e0b6ea01e3dc985fe06ed7d050b1d8fea7e50
kwak70@epozen.com <kwak70@epozen.com>
2020-02-07 16:56:45 +0900

    1

--- src/main/java/algorik5/dynamicsql/DynamicController.java
+++ src/main/java/algorik5/dynamicsql/DynamicController.java
@@ -6,10 +6,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,13 +25,25 @@ public class DynamicController {
 	 private static Logger logger = LoggerFactory.getLogger(DynamicController.class);
 
 	 @Autowired
+	 Environment env;
+	 
+	 @Autowired
 	 private DynamicService dbService;
 
 	 
 	 @GetMapping("/dynamicSelect")
-	 @CrossOrigin(origins = "http://localhost:4200")//show tables
-	 public List<Map<String,Object>> dynamicSelect(@RequestParam(required=false,defaultValue="select * from tab") String sql,@RequestParam(required=false,defaultValue="5") int rownum)
+	 @CrossOrigin(origins = "http://localhost:4200")
+	 public List<Map<String,Object>> dynamicSelect(@RequestParam(required=false,defaultValue="") String sql,@RequestParam(required=false,defaultValue="5") int rownum) throws Exception
 	 {
+		 if(StringUtils.length(sql)<1)
+		 {
+			 String driver = env.getProperty("spring.datasource.driver-class-name");
+			 if(StringUtils.contains(driver, ".h2.")) sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
+			 else if(StringUtils.contains(driver, "OracleDriver")) sql = "select * from tab";
+			 else throw new Exception("--- sql required ---");
+			 logger.info("##### DEFAULT SQL - "+ "#driver="+ driver+ "#sql="+ sql);
+		 }
+		 
 		 logger.info("======================= dynamicSelect START !! "+ "#rownum="+ rownum+ "#sql="+ sql);
 		 
 		 Map<String,Object> bindingMap =  new LinkedHashMap<String, Object>();
